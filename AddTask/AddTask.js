abd336fb664a446b980a8b14c9d54cef6014189d
Arthur <arthureuw@gmail.com>
2020-12-15 22:46:42 +0100

    Added UI Library #NoFilter and play around with it :)

--- AddTask/AddTask.js
+++ AddTask/AddTask.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Text, View, Button} from "react-native";
+import {View} from "react-native";
+import {Button, Text} from 'native-base';
 
 const AddTask = ({navigation}) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Here you can add a Task ! 🥳</Text>
             <Button
                 title="Go to Details"
-                onPress={() => navigation.navigate('AddTask')}
+                onPress={() => navigation.goBack()}
             />
         </View>
     );
--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {Button} from "react-native";
+import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Button, Icon, Text} from 'native-base';
 
 import AddTask from "./AddTask/AddTask";
 
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Home Screen</Text>
-            <Button
-                title="Click here to add a brand new task"
-                onPress={() => navigation.navigate('AddTask')}
-            />
+            <Button iconCenter bordered primary style={{marginLeft: 280, marginTop: 500}}
+                    onPress={() => navigation.navigate('AddTask')}>
+                <Icon name='add' primary/>
+            </Button>
         </View>
     );
 }
@@ -21,14 +21,14 @@ function HomeScreen({ navigation }) {
 const Stack = createStackNavigator();
 
 function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="AddTask" component={AddTask} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="AddTask" component={AddTask}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
